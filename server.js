9688ab42a1f25b0b1b065e98cc1b6ac57951fe55
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-08-25 15:21:23 +0900

    feat: built traditional frontend by using ejs framework

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -15,15 +15,21 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end("<h1>HELLO WORLD</h1>");
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1>You are in gifts page</h1>");
+// app.get("/hello", function (req, res) {
+//   res.end("<h1>HELLO WORLD</h1>");
+// });
+// app.get("/gift", function (req, res) {
+//   res.end("<h1>You are in gifts page</h1>");
+// });
+app.get("/", function (req, res) {
+  res.render("harid");
 });
-
 const server = http.createServer(app);
-let PORT = 3000;
+let PORT = 8080;
 server.listen(PORT, function () {
   console.log(`The serevr is running successfully on port: ${PORT}`);
 });
