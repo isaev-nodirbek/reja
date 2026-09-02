98318703451593f94f44e9a087fd30f3f76906dd
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-02 13:41:36 +0900

    feat: completed MIT TASK - C

--- train.js
+++ train.js
@@ -1,21 +1,74 @@
-// TASK - B
+// TASK - C
 /*
-Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
-MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-*/
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
-function countDigits(string) {
-  let number = 0;
-  for (let a = 0; a < string.length; a++) {
-    if (string[a] >= "0" && string[a] <= "9") {
-      number++;
-    }
+@MITASK
+*/
+const moment = require("moment");
+class Shop {
+  constructor(bread, lagman, cola) {
+    this.bread = bread;
+    this.lagman = lagman;
+    this.cola = cola;
   }
-  return number;
+  qoldiq() {
+    const now = new Date();
+    const time = moment(now).format("HH:mm");
+    console.log(
+      `At ${time}, There are ${this.bread} bread, ${this.lagman} lagman and ${this.cola} cola!`,
+    );
+  }
+
+  sell(product, quantity) {
+    if (product === "bread") {
+      this.bread -= quantity;
+    } else if (product === "lagman") {
+      this.lagman -= quantity;
+    } else if (product === "cola") {
+      this.cola -= quantity;
+    } else {
+      console.log("Product not found");
+    }
+    const now = new Date();
+    const time = moment(now).format("HH:mm");
+    console.log(`At ${time}, ${quantity} ${product} sold.`);
+  }
+
+  receive(product, quantity) {
+    if (product === "bread") {
+      this.bread += quantity;
+    } else if (product === "lagman") {
+      this.lagman += quantity;
+    } else if (product === "cola") {
+      this.cola += quantity;
+    } else {
+      console.log("Product not found");
+    }
+    const now = new Date();
+    const time = moment(now).format("HH:mm");
+    console.log(`At ${time}, ${quantity} ${product} received.`);
+  }
 }
 
-const answer = countDigits("jfbhsvfhjsv34h3ouyruegifgyid");
-console.log("Answer: ", answer);
+const shop = new Shop(10, 5, 2);
+shop.qoldiq();
+shop.sell("bread", 4);
+shop.receive("cola", 4);
+shop.qoldiq();
+
+// function countDigits(string) {
+//   let number = 0;
+//   for (let a = 0; a < string.length; a++) {
+//     if (string[a] >= "0" && string[a] <= "9") {
+//       number++;
+//     }
+//   }
+//   return number;
+// }
+
+// const answer = countDigits("jfbhsvfhjsv34h3ouyruegifgyid");
+// console.log("Answer: ", answer);
 
 // TASK - A
 // Harf sifatida kiritilgan birinchi parametr,
