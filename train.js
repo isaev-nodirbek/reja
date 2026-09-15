b1c4704f71e1712f788fd7aa36ee85ed04fac415
Nodirbek Isaev <nodirbekisaev5@gmail.com>
2026-09-15 14:50:57 +0900

    feat: MIT TASK- G is done

--- train.js
+++ train.js
@@ -1,17 +1,65 @@
-// TASK E:
+// TASK G:
 
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+function getHighestIndex(arr) {
+  let maxIndex = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[maxIndex]) {
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+const result = getHighestIndex([5, 21, 12, 21, 8]);
+console.log("Result: ", result);
+
+// TASK F:
+
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin.
+
+// MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+// function findDoublers(str) {
+//   let letters = [];
+
+//   for (let a = 0; a < str.length; a++) {
+//     if (letters.includes(str[a])) {
+//       return true;
+//     }
+
+//     letters.push(str[a]);
+//   }
+
+//   return false;
+// }
+
+// const result = findDoublers("Hello");
+// console.log("Result: ", result);
+
+// TASK E:
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
 
-function getReverse(str) {
-  let result = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i];
-  }
-  return result;
-}
-const result = getReverse("hello");
-console.log("Result: ", result);
+// function getReverse(str) {
+//   let result = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+// const result = getReverse("hello");
+// console.log("Result: ", result);
 
 // TASK - D
 // Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
